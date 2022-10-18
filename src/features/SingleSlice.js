14819c2979e31e7fd04bbad26fee82d2e8400de9
EBILITE <ebilitenelson@gmail.com>
2022-10-18 15:53:31 +0100

    singleSlice store setup

---
+++ src/features/SingleSlice.js
@@ -0,0 +1,39 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const singleArtItems = createAsyncThunk(
+  "singleArt/singleArtItems",
+  async (url) => {
+    try {
+      const resp = await fetch(url);
+      return await resp.json();
+    } catch (err) {
+      return console.log(err);
+    }
+  }
+);
+
+const initialState = {
+  singleItem: [],
+  isLoading: false,
+};
+
+const SingleArtSlice = createSlice({
+  name: "singleArt",
+  initialState,
+
+  extraReducers: {
+    [singleArtItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [singleArtItems.fulfilled]: (state, action) => {
+      const { data } = action.payload;
+      state.isLoading = false;
+      state.singleItem = data;
+    },
+    [singleArtItems.rejected]: (state) => {
+      state.isLoading = false;
+    },
+  },
+});
+
+export default SingleArtSlice.reducer;

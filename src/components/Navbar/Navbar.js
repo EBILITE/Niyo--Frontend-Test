a972b3df45cb17789b68ff23bc27448f8cb5a27c
EBILITE <ebilitenelson@gmail.com>
2022-10-18 12:47:43 +0100

    Implemented the Navbar and NavbarData.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Logo from "../../assests/images/Logo.svg";
+import { NavData } from "../../utils/NavbarData";
+// import Button from "../Button/Button";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Wrapper>
+      <div className="section-center">
+        <img src={Logo} alt="" />
+        <div className="NavData-Flex">
+          {NavData.map((items) => {
+            return <p key={items.id}>{items.name}</p>;
+          })}
+          {/* <Button text="Login" link="/" /> */}
+          {/* <Button text="Explore Art" link="/allarts" /> */}
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
-export default Navbar
+export default Navbar;
+
+const Wrapper = styled.div`
+  /* border: 2px solid red; */
+  margin: 0 auto;
+  height: 5rem;
+  .section-center {
+    /* border: 2px solid blue; */
+    width: 95vw;
+    margin: 0 auto;
+    max-width: var(--max-width);
+    display: flex;
+    align-items: center;
+    height: 100%;
+    justify-content: space-between;
+  }
+  .NavData-Flex {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    p {
+      color: var(--primary-50);
+    }
+  }
+  .sec1,
+  .sec2 {
+    margin-top: 0.3rem;
+  }
+`;
---
+++ src/utils/NavbarData.js
@@ -0,0 +1,6 @@
+export const NavData = [
+  { id: 1, name: "Events", link: "*" },
+  { id: 2, name: "Museum", link: "*" },
+  { id: 3, name: "Arts", link: "*" },
+  { id: 4, name: "Galleries", link: "*" },
+];

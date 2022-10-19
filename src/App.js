e2f6650ec08ccbaeb3591d893bfa337ae6e0f80e
EBILITE <ebilitenelson@gmail.com>
2022-10-19 08:47:09 +0100

    Error Page Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import AllArts from "./Pages/AllArts/AllArts";
+import Error from "./Pages/Error";
 import LandingPage from "./Pages/LandingPage";
 import SingleArt from "./Pages/singleArt";
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="allarts" element={<AllArts />} />
         <Route path="single/:id" element={<SingleArt />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   );
---
+++ src/Pages/Error.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components";
+import { BiArrowBack } from "react-icons/bi";
+import { Link } from "react-router-dom";
+
+const Error = () => {
+  return (
+    <Wrapper>
+      <div className="Error">
+        <Link to="/" className="link">
+          <BiArrowBack />
+          <p>Back</p>
+        </Link>
+        <div className="ErrorText">
+          <h1>Page Not Found</h1>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Error;
+
+const Wrapper = styled.div`
+  /* border: 2px solid white; */
+  margin: 0 auto;
+  /* height: 100vh; */
+  margin-top: 2rem;
+  .Error {
+    /* border: 2px solid blue; */
+    width: 95vw;
+    max-width: var(--max-width);
+    margin: 0 auto;
+  }
+  h1, p {
+    color: white;
+  }
+  .link {
+    display: flex;
+    gap: 1rem;
+    svg {
+      font-size: 44px;
+      color: white;
+    }
+  }
+  .ErrorText{
+    display: flex;
+    align-items: center;
+justify-content: center;
+  }
+`;
